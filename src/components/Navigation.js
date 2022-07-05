7d47191e94adeac503759accf29356cc48ff839d
alexnguyen2201 <n.vanson.2201@gmail.com>
2022-07-05 15:02:21 +0700

    remove link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,7 +12,6 @@ function Navigation() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">
                                 Action
